3db7a6b08d669cfb6acd578598e63d185041bdc5
DHarper04 <dharpe11@my.westga.edu>
2023-02-04 22:08:09 -0500

    Created Login Page UI (Web) and added login credential validation
    Created functionality that validates the login credentials that are inputted on the login screen. A prompt returns information to the user on whether the inputted credentials were correct or not.

---
+++ code/RecipePlannerApplication/RecipePlannerWebApplication/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RecipePlannerWebApplication.Models;
+using System.Diagnostics;
+
+namespace RecipePlannerWebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index([Bind] Login ad)
+        {
+            int res = Database.LoginCheck(ad);
+            if (res == 1)
+            {
+                TempData["msg"] = "Successful Login!";
+            }
+            else
+            {
+                TempData["msg"] = "The Username or Password is incorrect.";
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ code/RecipePlannerApplication/RecipePlannerWebApplication/Models/Database.cs
@@ -0,0 +1,34 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using MySql.Data.MySqlClient;
+
+namespace RecipePlannerWebApplication.Models
+{
+    public class Database
+    {
+        private static MySqlConnection con = new MySqlConnection(Connection.LoginConnectionString);
+        public static int LoginCheck(Login ad)
+        {
+            MySqlCommand com = new MySqlCommand("SELECT username FROM login WHERE username=@Username AND password=@Password", con);
+            com.Parameters.AddWithValue("@Username", ad.Username);
+            com.Parameters.AddWithValue("@Password", ad.Password);
+
+            con.Open();
+            MySqlDataReader reader = com.ExecuteReader();
+
+            int result = 0;
+            if (reader.Read())
+            {
+                result = 1;
+            }
+            else
+            {
+                result = 0;
+            }
+
+            con.Close();
+            return result;
+        }
+
+    }
+}
