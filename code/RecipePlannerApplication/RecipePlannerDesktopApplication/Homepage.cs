cd71ffce7f10b96546fc1f64ad219301c21996fa
JaneraSmith <jsmit189@my.westga.edu>
2023-02-06 13:34:44 -0500

    Made the Hompage UI

--- code/RecipePlannerApplication/RecipePlannerDesktopApplication/Homepage.cs
+++ code/RecipePlannerApplication/RecipePlannerDesktopApplication/Homepage.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace RecipePlannerDesktopApplication
 {
-    public partial class Form1 : Form
+    public partial class Homepage : Form
     {
-        public Form1()
+        public Homepage()
         {
             InitializeComponent();
         }
