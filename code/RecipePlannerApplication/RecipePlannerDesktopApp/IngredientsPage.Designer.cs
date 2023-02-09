f08b20fae58386381fdec1ab5ef04c30acce1d3c
DHarper04 <dharpe11@my.westga.edu>
2023-02-09 16:50:39 -0500

    Created Ingredient Page UI
    Added new form and UI components needed for owned ingredients page.

---
+++ code/RecipePlannerApplication/RecipePlannerDesktopApp/IngredientsPage.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace RecipePlannerDesktopApplication
+{
+    partial class IngredientsPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ingredientsListView = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ingredientsListView
+            // 
+            this.ingredientsListView.FormattingEnabled = true;
+            this.ingredientsListView.ItemHeight = 15;
+            this.ingredientsListView.Location = new System.Drawing.Point(25, 52);
+            this.ingredientsListView.Name = "ingredientsListView";
+            this.ingredientsListView.Size = new System.Drawing.Size(358, 334);
+            this.ingredientsListView.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(75, 411);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(264, 34);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add Ingredient";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(75, 461);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(264, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Remove Ingredient";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(25, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Owned Ingredients:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(325, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Logout";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // IngredientsPage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(412, 519);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ingredientsListView);
+            this.Name = "IngredientsPage";
+            this.Text = "IngredientsPage";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ListBox ingredientsListView;
+        private Button button1;
+        private Button button2;
+        private Label label1;
+        private Button button3;
+    }
+}
