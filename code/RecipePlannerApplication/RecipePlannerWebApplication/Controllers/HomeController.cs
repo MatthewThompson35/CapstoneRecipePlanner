22729970561dc6a3c9b6337a1991dc4798e72ecf
Matthew Thompson <mthomp35@my.westga.edu>
2023-02-24 21:21:37 -0500

    Refactored method in Controller to simplify code
    refactored methods in the controller that used logic to be implemented and extracted them to helper methods

--- code/RecipePlannerApplication/RecipePlannerWebApplication/Controllers/HomeController.cs
+++ code/RecipePlannerApplication/RecipePlannerWebApplication/Controllers/HomeController.cs
@@ -58,6 +58,16 @@ namespace RecipePlannerWebApplication.Controllers
                 recipe.Ingredients = RecipeDAL.getIngredientsForRecipe(recipe.RecipeId);
                 recipe.Steps = RecipeDAL.getStepsForRecipe(recipe.RecipeId);
             }
+            this.addToAvailableRecipes(recipes);
+            ViewBag.AvailableRecipes.Sort((Comparison<Recipe>)CompareRecipesByName);
+            ViewBag.AllRecipes.Sort((Comparison<Recipe>)CompareRecipesByName);
+        }
+        /// <summary>
+        /// Adds to the available recipes if the user can make that recipe.
+        /// </summary>
+        /// <param name="recipes">The recipes.</param>
+        private void addToAvailableRecipes(List<Recipe> recipes)
+        {
             ViewBag.AvailableRecipes = new List<Recipe>();
             ViewBag.AllRecipes = recipes;
             foreach (var recipe in ViewBag.AllRecipes)
@@ -86,8 +96,6 @@ namespace RecipePlannerWebApplication.Controllers
                     ViewBag.AvailableRecipes.Add(recipe);
                 }
             }
-            ViewBag.AvailableRecipes.Sort((Comparison<Recipe>)CompareRecipesByName);
-            ViewBag.AllRecipes.Sort((Comparison<Recipe>)CompareRecipesByName);
         }
 
         /// <summary>
@@ -112,17 +120,29 @@ namespace RecipePlannerWebApplication.Controllers
             var quantity = 0;
             var ingredientID = Int32.Parse(id);
 
+            quantity = getItemQuantity(ingredientID);
+            IngredientDAL.decrementQuantity(ingredientID, quantity);
+            ViewBag.ingredients = IngredientDAL.getIngredients();
+            return View("IngredientsPage", ViewBag.ingredients);
+        }
+
+        /// <summary>
+        /// Gets the item quantity.
+        /// </summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns></returns>
+        private int getItemQuantity(int id)
+        {
+            var quantity = 0;
             foreach (var item in ViewBag.ingredients)
             {
-                if (item.id == ingredientID)
+                if (item.id == id)
                 {
                     quantity = item.quantity;
 
                 }
             }
-            IngredientDAL.decrementQuantity(ingredientID, quantity);
-            ViewBag.ingredients = IngredientDAL.getIngredients();
-            return View("IngredientsPage", ViewBag.ingredients);
+            return quantity;
         }
 
         /// <summary>
@@ -136,14 +156,7 @@ namespace RecipePlannerWebApplication.Controllers
             var quantity = 0;
             var ingredientID = Int32.Parse(id);
 
-            foreach (var item in ViewBag.ingredients)
-            {
-                if (item.id == ingredientID)
-                {
-                    quantity = item.quantity;
-
-                }
-            }
+            quantity = getItemQuantity(ingredientID);
             IngredientDAL.incrementQuantity(ingredientID, quantity);
             ViewBag.ingredients = IngredientDAL.getIngredients();
             return View("IngredientsPage", ViewBag.ingredients);
