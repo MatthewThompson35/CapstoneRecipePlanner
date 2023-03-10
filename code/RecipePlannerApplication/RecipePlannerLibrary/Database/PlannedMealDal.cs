2636179e2d3423ecd667e742069fb4be3270dfc1
JaneraSmith <jsmit189@my.westga.edu>
2023-03-10 17:11:11 -0500

    Included more methods for the PlannedMealsDal.
    More methods are needed for the desktop side.

--- code/RecipePlannerApplication/RecipePlannerLibrary/Database/PlannedMealDal.cs
+++ code/RecipePlannerApplication/RecipePlannerLibrary/Database/PlannedMealDal.cs
@@ -63,6 +63,54 @@ namespace RecipePlannerLibrary.Database
 
             return nextWeeksMeals;
         }
+
+        public static void RemoveThisWeekMeal(string connectionString, int id, string day, string type, DateTime date)
+        {
+
+            using var connection = new MySqlConnection(connectionString);
+            connection.Open();
+            var query = @"DELETE FROM planned_recipe WHERE recipeID = @id and dayOfTheWeek = @day and mealType = @type and dateUsed = @date;";
+            using var command = new MySqlCommand(query, connection);
+            command.Parameters.Add("@id", MySqlDbType.Int32).Value = id;
+            command.Parameters.Add("@day", MySqlDbType.VarChar).Value = day;
+            command.Parameters.Add("@type", MySqlDbType.VarChar).Value = type;
+            command.Parameters.Add("@date", MySqlDbType.Date).Value = date;
+            using var reader = command.ExecuteReader();
+
+        }
+
+        public static Boolean exists(string connectionString, string type, DateTime date)
+        {
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                MySqlCommand command = new MySqlCommand("SELECT COUNT(*) > 0 FROM planned_recipe WHERE dateUsed = @date AND mealType = @type", connection);
+                command.Parameters.AddWithValue("@date", date);
+                command.Parameters.AddWithValue("@type", type);
+
+                long result = (long)command.ExecuteScalar();
+                bool exists = (result > 0);
+
+                connection.Close();
+
+                return exists;
+            }
+        }
+
+        public static void UpdateThisWeeksMeal(string connectionString, string day, string type, DateTime date,
+            int recipeId)
+        {
+            using var connection = new MySqlConnection(connectionString);
+            connection.Open();
+            var query = @"Update planned_recipe set recipeID = @id WHERE dayOfTheWeek = @day and mealType = @type and dateUsed = @date;";
+            using var command = new MySqlCommand(query, connection);
+            command.Parameters.Add("@id", MySqlDbType.Int32).Value = recipeId;
+            command.Parameters.Add("@day", MySqlDbType.VarChar).Value = day;
+            command.Parameters.Add("@type", MySqlDbType.VarChar).Value = type;
+            command.Parameters.Add("@date", MySqlDbType.Date).Value = date;
+            using var reader = command.ExecuteReader();
+        }
     }
     
 }
